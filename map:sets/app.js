fdacfb4832c24081370986ff8d448f553ce66293
Heather Demetroff <torunonothing@gmail.com>
2024-03-09 11:58:04 -0500

    initial commit of map/sets exercise

---
+++ map:sets/app.js
@@ -0,0 +1,34 @@
+// What does the following code return?
+[...new Set("referee")].join(""); // "ref"
+
+/*
+  0: {Array(3) => true}
+  1: {Array(3) => false}
+*/
+
+hasDuplicate([1,3,2,1]) // true
+hasDuplicate([1,5,-1,4]) // false
+
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+vowelCount('Colt') // Map { 'o' => 1 }
+
+function isVowel(char){
+  return "aeiou".includes(char);
+}
+
+function vowelCount(str){
+  const vowelMap = new Map();
+  for(let char of str){
+    let lowerCaseChar = char.toLowerCase()
+    if(isVowel(lowerCaseChar)){
+      if(vowelMap.has(lowerCaseChar)){
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      } else {
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
