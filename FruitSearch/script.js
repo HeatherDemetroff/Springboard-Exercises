45a3b31167f9cf5d48972ffb0484707556c0a2e3
Heather Demetroff <torunonothing@gmail.com>
2024-03-18 18:58:42 -0400

    updated fruit search project. updated css & js

--- FruitSearch/script.js
+++ FruitSearch/script.js
@@ -6,7 +6,6 @@ const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Black
 // a function that searches the fruit array and returns a result
 function search(str) {
 	let results = [];
-	console.log(str);
 	// loops through all fruits and adds them to results if user input is included in fruit list
 	fruits.forEach(fruit => {
 		if (fruit.toLowerCase().includes(str.toLowerCase())) {
@@ -23,25 +22,15 @@ function searchHandler(e) {
 	let searchBoxVal = searchBox.value;
 	const dropdown = document.querySelector(".suggestions");
 
+	
 	if (searchBoxVal === '') {
-		// If the search box is empty, hide the dropdown
+		ul.innerHTML = '';
 		dropdown.style.display = 'none';
 	} else {
 		let results = search(searchBoxVal);
 		console.log(results);
 		showSuggestions(results, searchBoxVal);
-		// Show the dropdown
 		dropdown.style.display = 'block';
-	}
-	// if search box is empty, clear the results
-	if (searchBoxVal === '') {
-		ul.innerHTML = '';
-	} else {
-		// if search box is not empty, perform search
-		let results = search(searchBoxVal);
-		console.log(results);
-		// show search results in the dropdown
-		showSuggestions(results, searchBoxVal);
 	}
 }
 // function that shows the search results in the dropdown
@@ -60,13 +49,11 @@ function showSuggestions(results, inputVal) {
 // a function that populates the search box with the clicked suggestion
 function useSuggestion(e) {
 	const searchBox = document.getElementById("fruit");
-	console.log(e);
 	searchBox.value = e.target.innerText;
 }
 
 // event listener that handles clicks of the dropdown
 const dropdown = document.querySelector(".suggestions");
-dropdown.addEventListener('click', useSuggestion);
 
 dropdown.addEventListener('mouseleave', function () {
 	// When the mouse leaves the dropdown, hide the dropdown
